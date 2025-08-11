c4b42fd1806f00254c42b33ac1f5a07d143e0e25
Gabriel Frais <gfrais22@gmail.com>
2025-08-11 16:50:28 -0300

    Sorteando um nome

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function adicionarAmigo(){
     let nomeInput = document.getElementById('amigo')
     let nome = nomeInput.value;
     if (nome == ''){
-        alert("Por favor informe um nome");
+        alert("Por favor, informe um nome!");
     }else{
         listaAmigos.push(nome);
         nomeInput.value = '';
@@ -20,6 +20,19 @@ function mostrarListaAmigos(){
     for (let i = 0; i < listaAmigos.length; i++){
         const li = document.createElement('li');
         li.textContent = listaAmigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    if (listaAmigos.length < 1){
+        alert("Nenhum amigo na lista!");
+    }else{
+        let indiceNome = Math.floor(Math.random() * listaAmigos.length);
+        let lista = document.getElementById('resultado');
+        lista.innerHTML = "";
+        const li = document.createElement('li');
+        li.textContent = listaAmigos[indiceNome];
         lista.appendChild(li);
     }
 }
