21cef31b64bdc158abf2aa8b3f3d0cde81c417d6
Gabriel Frais <gfrais22@gmail.com>
2025-08-11 16:20:30 -0300

    Adicionando os nomes na lista e verificando se está vazio

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaAmigos = [];
+
+function adicionarAmigo(){
+    let nomeInput = document.getElementById('amigo')
+    let nome = nomeInput.value;
+    if (nome == ''){
+        alert("Por favor informe um nome");
+    }else{
+        listaAmigos.push(nome);
+        nomeInput.value = '';
+    }
+}
